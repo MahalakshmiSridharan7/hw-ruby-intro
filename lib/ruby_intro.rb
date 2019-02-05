9ef44a6f33058a459cc840afbfdb2d5a79bfb3f7
EC2 Default User <ec2-user@ip-172-31-5-57.us-east-2.compute.internal>
2019-02-05 19:49:02 +0000

    HW1 Done

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,55 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  #Sum elements of array
+  arr.inject(0, :+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  #Select the maximum 2 values of array and add them
+  arr.max(2).reduce(0,:+)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  #Pop each array element and check if the rest of the array contains (target-popped element)
+  while num=arr.pop do
+    return true if arr.member?(n-num)
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, "+name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  #check if the string begins with an alphabet but not a vowel
+  s =~ /\A(?=[^aeiou])(?=[a-z])/i
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  #check if string is binary and multiple of 4
+  s =~ /(^0|^[10]*00)$/
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  
+  #getters and setters
+  attr_accessor :isbn, :price
+  
+  #constructor; raise exception for invalid isbn and price
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+  
+  #format price with $ and 2 decimal places
+  def price_as_string
+    "$%.2f" % @price
+  end
 end
